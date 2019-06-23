f699314500c66352e9252575c256289fce803039
resident234 <H4vx_EApAcyuxWWec3T->
2019-06-24 01:05:29 +1000

    small additional

--- server.py
+++ server.py
@@ -1,11 +1,17 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 app = Flask(__name__)
 
+result = [
+    {
+        'id': 1,
+        'code': "default code"
+    }
+]
 
-@app.route("/")
+
+@app.route("/api/v1.0/s/", methods=['POST'])
 def hello():
-    return "Hello World! "
-
+    return jsonify({'result': request})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
