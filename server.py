e598010ec800d4d57641e4e68844c0ba08fdacd4
resident234 <H4vx_EApAcyuxWWec3T->
2019-06-24 02:43:39 +1000

    small additional @2

--- server.py
+++ server.py
@@ -9,9 +9,15 @@ result = [
 ]
 
 
-@app.route("/api/v1.0/s/", methods=['POST'])
-def hello():
-    return jsonify({'result': request})
+@app.route('/api', methods=['GET', 'POST'])
+def main():
+    if request.method == 'GET':
+        return 'GET'
+    elif request.method == 'POST':
+        return jsonify({'result': request})
+    else:
+        return "ok"
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
