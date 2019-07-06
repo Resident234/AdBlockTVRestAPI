fbed31b320279152650d00a1f593c887404dfc3f
GSU <gsu1234@mail.ru>
2019-07-07 02:11:02 +0900

    added db classes

--- server.py
+++ server.py
@@ -1,25 +1,27 @@
 from flask import Flask, jsonify, request
-import logging
+import json
+from database import get_database, Database
 
 app = Flask(__name__)
-
-result = [
-    {
-        'id': 1,
-        'code': "default code"
-    }
-]
 
 
 @app.route('/api', methods=['GET', 'POST'])
 def main():
+    with open("db.cnf") as f:
+        config = json.load(f)
+
+    # initialize db
+    db_cls = get_database(config.get("database_type", None))
+    db = db_cls(**config.get("database", {}))
+    db.setup()
+
     if request.method == 'GET':
         return request.args.get('code')
     elif request.method == 'POST':
         json = request.get_json()
         if len(json['code']) == 0:
             return jsonify({'error': 'invalid input'})
-
+        db.return_matches(json['code'])
         return jsonify({'you sent this': json['code']})
     else:
         return "ok"
