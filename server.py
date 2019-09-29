e466c45bcaf8ad95041e2e698b1c0af2ff941373
resident234 <H4vx_EApAcyuxWWec3T->
2019-09-29 21:00:31 +1000

    merged get and post handlers

--- server.py
+++ server.py
@@ -8,6 +8,8 @@ from collections import defaultdict
 
 from dejavu import Dejavu
 import dejavu.logger as logger
+import ast
+
 
 with open("dejavu.cnf") as f:
     config = json.load(f)
@@ -25,41 +27,32 @@ def main():
 
     if request.method == 'GET':
         file_log = open("hashes_samples.log", "r")
-        fl = file_log.readlines()
-        hashes = dict()
-        samples_indexes = []
-        for string in fl:
-            sample_index, hash_local, offset = string.rstrip().split('|')
-            if sample_index in hashes:
-                hashes[sample_index].append((hash_local, int(offset)))
-            else:
-                hashes[sample_index] = []
-            if sample_index not in samples_indexes:
-                samples_indexes.append(sample_index)
-        file_log.close()
-
-        matches = []
-        for sample_index in samples_indexes:
-            matches.extend(djv.rest_find_matches(hashes[sample_index]))
-
-        return str(djv.align_matches(matches))
-        return "count records in db: %s" % djv.get_num_songs()
+        array_hashes = file_log.readlines()
     elif request.method == 'POST':
         json_local = request.get_json()
-        f = open("dict.txt", "w")
-        f.write(str(json_local))
-        f.close()
-        if len(json_local['code']) == 0:
+        if len(json_local['hashes']) == 0:
             return jsonify({'error': 'invalid input'})
 
-        matches = []
-        matches.extend(djv.find_hashes(json['code']))
-        return jsonify({'result': djv.align_matches(matches)})
+        string_hashes = json_local['hashes']
+        string_hashes = string_hashes.replace("[", "")
+        string_hashes = string_hashes.replace("]", "")
+        array_hashes = string_hashes.rstrip().split(', ')
 
-        # return jsonify({'you sent this': json_local['code']})
-    else:
-        return "ok"
+    hashes = dict()
+    samples_indexes = []
+    for string_hash in array_hashes:
+        sample_index, hash_local, offset = string_hash.rstrip().split('|')
+        if sample_index in hashes:
+            hashes[sample_index].append((hash_local, int(offset)))
+        else:
+            hashes[sample_index] = []
+        if sample_index not in samples_indexes:
+            samples_indexes.append(sample_index)
 
+    matches = []
+    for sample_index in samples_indexes:
+        matches.extend(djv.rest_find_matches(hashes[sample_index]))
+    return jsonify(djv.align_matches(matches))
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
